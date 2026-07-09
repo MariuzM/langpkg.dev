f26e0456881955f02e1509df6400df5e60795d71
MariuzM <me@marius.dev>
2026-07-09 14:28:24 +0700

    Maxduration

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,7 @@ const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    nitro({
-      rollupConfig: { external: [/^@sentry\//] },
-      vercel: { functions: { maxDuration: 800 } },
-    }),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
